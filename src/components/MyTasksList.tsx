840415453f5eb55dacc48e94ecaadda0e75fcc44
cybercris <crssoares.js@gmail.com>
2021-06-15 19:01:57 -0300

    handle tasks management

--- src/components/MyTasksList.tsx
+++ src/components/MyTasksList.tsx
@@ -30,13 +30,18 @@ export function MyTasksList({ tasks, onLongPress, onPress }: MyTasksListProps) {
             testID={`button-${index}`}
             activeOpacity={0.7}
             //TODO - use onPress, onLongPress and style props
+            onPress={() => onPress(item.id)}
+            onLongPress={() => onLongPress(item.id)}
+            style={!item.done ? styles.taskButton : styles.taskButtonDone}
           >
             <View 
               testID={`marker-${index}`}
               //TODO - use style prop 
+              style={!item.done ? styles.taskMarker : styles.taskMarkerDone}
             />
             <Text 
               //TODO - use style prop
+              style={!item.done ? styles.taskText : styles.taskTextDone}
             >
               {item.title}
             </Text>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Header } from '../components/Header';
-import { MyTasksList } from '../components/MyTasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { MyTasksList } from "../components/MyTasksList";
+import { TodoInput } from "../components/TodoInput";
 
 interface Task {
   id: number;
@@ -11,18 +11,39 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if (newTaskTitle.length !== 0) {
+      setTasks((oldState) => [
+        ...oldState,
+        {
+          id: new Date().getTime(),
+          title: newTaskTitle,
+          done: false,
+        },
+      ]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+    const newTaskList = tasks.map(task => {
+      if(task.id === id){
+        task.done = !task.done
+      }
+
+      return task;
+    })
+
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const newTaskList = tasks.filter((task) => task.id !== id);
+    setTasks(newTaskList);
   }
 
   return (
@@ -31,11 +52,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
